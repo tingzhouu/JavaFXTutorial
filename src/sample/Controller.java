421eebb81c6f56f8fc0e53a732a58222efcfdd59
Ting Zhou <limtingzhou@gmail.com>
2018-11-29 23:08:01 +0800

    Add button for Event Handler
    This commit adds a button which will output the String ("Hello") with the input provided by user in the textbox.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,16 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    public void onButtonClicked() {
+        System.out.println("Hello, " + nameField.getText());
+    }
+
+
 }
