2c2bd4d8c46d0569faffa49db2d04742b7cdcc1f
Ting Zhou <limtingzhou@gmail.com>
2018-12-01 00:12:49 +0800

    Add CheckBox for clearing TextBox; Introduce thread sleep
    The CheckBox will indicate if the user wants to clear the text after clicking the button
    If checked, TextBox will be empty after button is clicked.
    
    The thread will also sleep for 3 seconds after the button is clicked.
    This is to simulate how an application can hang.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 
 public class Controller {
@@ -12,6 +13,8 @@ public class Controller {
     private Button helloButton;
     @FXML
     private Button byeButton;
+    @FXML
+    private CheckBox clearTextCheckBox;
 
     @FXML
     public void initialize() {
@@ -21,13 +24,25 @@ public class Controller {
 
     @FXML
     public void onButtonClicked(ActionEvent e) {
+
+
         if (e.getSource().equals(helloButton)) {
-
             System.out.println("Hello, " + nameField.getText());
-
         } else if (e.getSource().equals(byeButton)) {
-
             System.out.println("Bye, " + nameField.getText());
+        }
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException exception) {
+
+        }
+
+
+        if (clearTextCheckBox.isSelected()) {
+            nameField.clear();
+            helloButton.setDisable(true);
+            byeButton.setDisable(true);
         }
 
         System.out.println("The following button was pressed" + e.getSource());
@@ -39,6 +54,11 @@ public class Controller {
         boolean disableButtons = text.isEmpty() || text.trim().isEmpty();
         helloButton.setDisable(disableButtons);
         byeButton.setDisable(disableButtons);
+    }
+
+
+    public void handleChange() {
+        System.out.println("The checkbox is " + (clearTextCheckBox.isSelected() ? "checked" : "not checked"));
     }
 
 
